c9fd2e6e3c623ff17ecf47d052522457495435bd
DevonDiianni <devondiianni@gmail.com>
2020-06-09 15:55:48 -0400

    Created new unsigned array type.

---
+++ src/unsigned_array.ads
@@ -0,0 +1,15 @@
+--------------------------------------------------------------------------------
+-- FILE   : unsigned_array.ads
+-- SUBJECT: Specification of a package for ...
+-- AUTHOR : (C) Copyright 2020 by Vermont Technical College
+--
+--------------------------------------------------------------------------------
+pragma SPARK_Mode(On);
+
+with Interfaces; use Interfaces;
+
+package Unsigned_Array is
+
+   type Unsigned_32_Array is array (Natural range <>) of Unsigned_32;
+
+end Unsigned_Array;
